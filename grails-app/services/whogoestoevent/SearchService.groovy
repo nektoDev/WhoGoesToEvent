e504b509bcdf4296f435a5d612806fadb28770c3
NektoDev <ts.slawa@gmail.com>
2014-01-16 23:40:33 +0400

    Add age filter to service + some fixes

--- grails-app/services/whogoestoevent/SearchService.groovy
+++ grails-app/services/whogoestoevent/SearchService.groovy
@@ -15,22 +15,22 @@ class SearchService implements Serializable{
     def initUserList(Filter filter) {
         vkUserList = new ArrayList<>();
         filteredList = new ArrayList<>();
+        offset = 0;
 
         def response = vkApiService.groupsGetMembers(filter.eventID, 1);
+
         Integer count = response.count as Integer;
         def i;
-        while (vkUserList.size() <= count) {
+        while (vkUserList.size() < count) {
             i = count - vkUserList.size();
             Integer loadingCount = i > 1000 ? 1000 : i;
 
             List<VkUser> members = vkApiService.getGroupsMembers(filter.eventID, loadingCount, vkUserList.size())
-
             for (VkUser member : members) {
                 if (member.compareWithFilter(filter)) {
                     filteredList.add(member);
                 }
             }
-
             vkUserList.addAll(members);
             Thread.sleep(350);
         }
@@ -38,9 +38,11 @@ class SearchService implements Serializable{
 
     public List<VkUser> getNextPage(Integer count = 15) {
         List<VkUser> result = new ArrayList<VkUser>();
-
-        result.addAll(filteredList[offset..offset+count-1]);
-        println result;
+        def to = offset + count;
+        if (filteredList.size() > offset) {
+            to = filteredList.size() < to ? filteredList.size() : to
+            result.addAll(filteredList[offset..to-1])
+        };
         offset += result.size();
         return result;
     }
--- grails-app/services/whogoestoevent/VkApiService.groovy
+++ grails-app/services/whogoestoevent/VkApiService.groovy
@@ -1,5 +1,4 @@
 package whogoestoevent
-
 import groovyx.net.http.HTTPBuilder
 /**
  * Created with IntelliJ IDEA.
@@ -16,7 +15,7 @@ class VkApiService {
     public static final String ACCESS_TOKEN="e00ca82c9ec6d4b285615f0b770d8e5bb5f21c600440901620483743159b303f4997dbb5cd1a5734b260c"
 
     List<VkUser> getGroupsMembers(String id, Integer count = 1000, Integer offset = 0) {
-        http.get(path: 'execute.groupsMembers', query: [group_id: id, count: count, offset: 0, access_token: ACCESS_TOKEN])
+        http.get(path: 'execute.groupsMembers', query: [group_id: id, count: count, offset: offset, access_token: ACCESS_TOKEN])
                 { resp, json ->
                     def users = json.response;
                     List<VkUser> result = new ArrayList<>();
@@ -30,7 +29,7 @@ class VkApiService {
 
 
     def groupsGetMembers(String id, Integer count = 1000, Integer offset = 0) {
-        http.get(path: 'groups.getMembers', query: [group_id: id, count: count, offset: 0, order: 'time_asc'])
+        http.get(path: 'groups.getMembers', query: [group_id: id, count: count, offset: 0, sort: 'time_asc'])
                 { resp, json ->
                     return json.response;
                 }
@@ -45,7 +44,18 @@ class VkApiService {
         vkUser.sex = userJSON.sex;
         vkUser.photo200 = userJSON.photo_200;
         vkUser.city = userJSON.city;
-
+        vkUser.birthdate = convertDate(userJSON.bdate);
+        vkUser.age = vkUser.birthdate?.age;
         return vkUser;
+    }
+
+    private Date convertDate(String date) {
+        if (date == null || date.isEmpty()) {
+            return null;
+        } else {
+            if (date.matches($/\d{1,2}\.\d{1,2}\.\d{4}/$)) {
+               return new Date().parse("d.M.yyyy", date);
+            }
+        }
     }
 }
